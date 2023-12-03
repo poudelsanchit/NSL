3b79acd4b73b366686f2792a1fe7228ba2921f2e
Sanchit Poudel <snchtpdl12@gmail.com>
2023-12-03 21:57:41 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-screen bg-black text-white text-xl  sm:text-4xl font-Roboto ">
+        <div className='p-5'>       Welcome To NSL Fantasy.....
+</div>
+      </div>
+    </>
+  );
+}
+
+export default App
